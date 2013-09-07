0be88c232b1abf4872bc1c55c8fd3e6200ddcba0
ZackBoe <zacknut@gmail.com>
2013-09-06 20:36:14 -0400

    Sprited icons
    I was bored, okay?

--- app/views/home.php
+++ app/views/home.php
@@ -1,74 +1,62 @@
 <?php echo $header; ?>
 
 	<section class="features wrap">
-		<article class="col3">
-			<img src="<?php echo asset('assets/img/icons/markdown.png'); ?>">
+		<article class="markdown col3">
 			<h2>Markdown-happy</h2>
 			<p>Anchor gives you full freedom over your words. Just write in <a href="http://daringfireball.net/projects/markdown/">Markdown</a> or HTML, whatever you prefer.</p>
 		</article>
 
-		<article class="col3">
-			<img src="<?php echo asset('assets/img/icons/tiny.png'); ?>">
+		<article class="tiny col3">
 			<h2>Teeny-tiny</h2>
 			<p>Prefer your blog systems without the bloat? Anchor weighs in smaller than a standard JPG image (~150kb).</p>
 		</article>
 
-		<article class="col3">
-			<img src="<?php echo asset('assets/img/icons/drag-and-drop.png'); ?>">
+		<article class="drag-and-drop col3">
 			<h2>Drag-n-drop</h2>
 			<p>Want to add some custom CSS, JavaScript, or an image to your post? Just drag the file over to Anchor, and we'll do the rest.</p>
 		</article>
 
-		<article class="col3">
-			<img src="<?php echo asset('assets/img/icons/open-source.png'); ?>">
+		<article class="open-source col3">
 			<h2>Open-source</h2>
 			<p>See something you want to improve in Anchor? Everything’s hosted on <a href="//github.com/anchorcms/anchor-cms">GitHub</a>, and everyone’s welcome to help and contribute.</p>
 		</article>
 
-		<article class="col3">
-			<img src="<?php echo asset('assets/img/icons/i18n.png'); ?>">
+		<article class="i18n col3">
 			<h2>Works in your language</h2>
 			<p>Anchor is fully <a href="http://www.debian.org/doc/manuals/intro-i18n/">i18n</a> compatible, so you can just grab a language file and get speaking Anchor in no time.</p>
 		</article>
 
-		<article class="col3">
-			<img src="<?php echo asset('assets/img/icons/themes.png'); ?>">
+		<article class="themes col3">
 			<h2>Super-simple theming</h2>
 			<p>Creating and modifying a theme is as simple as knowing some very simple PHP and HTML/CSS.</p>
 		</article>
 
-		<article class="col3">
-			<img src="<?php echo asset('assets/img/icons/events.png'); ?>">
+		<article class="events col3">
 			<h2>Event-bound</h2>
 			<p>Create and utilise custom theme hooks without the need for advanced programming knowledge.</p>
 		</article>
 
-		<article class="col3">
-			<img src="<?php echo asset('assets/img/icons/install.png'); ?>">
+		<article class="install col3">
 			<h2>Painfully easy to install</h2>
 			<p>Used to the ol’ famous five-minute install? Well, get ready for Anchor’s tantalising two-minute install.</p>
 		</article>
 
-		<article class="col3">
-			<img src="<?php echo asset('assets/img/icons/extensible.png'); ?>">
+		<article class="extensible col3">
 			<h2>Extensible</h2>
 			<p>You can add or remove an infinite amount of custom fields to any post or page at the click of a button.</p>
 		</article>
 
-		<article class="col3">
-			<img src="<?php echo asset('assets/img/icons/art-directed.png'); ?>">
+		<article class="art-directed col3">
 			<h2>Art-directed blogging</h2>
 			<p>Make every article unique with the ability to add custom CSS, JavaScript, and HTML to any blog post or page.</p>
 		</article>
 
-		<article class="col3">
-			<img src="<?php echo asset('assets/img/icons/free.png'); ?>">
+		<article class="free col3">
 			<h2>Forever free</h2>
 			<p>Anchor has been, is, and always will be 100% free of charge to everyone.</p>
 		</article>
 
-		<article class="col3">
-			<img src="<?php echo asset('assets/img/icons/modern.png'); ?>">
+		<article class="modern col3">
 			<h2>Markup made modern</h2>
 			<p>To keep Anchor small, we only support the latest browsers &mdash; no more IE6/7/8.</p>
 		</article>
